2c76b4aa67993da5ed5bc654b8ba89b97f800498
jangracner <jangracner@gmail.com>
2026-01-03 18:29:51 +0100

    Testing CI/CD for posts microservice

--- posts/src/postsService.ts
+++ posts/src/postsService.ts
@@ -2,13 +2,22 @@ import { Post } from './postsModels';
 
 export class PostService {
     async getPosts(): Promise<Post[]> {
-        const mockPost: Post = {
+        const mockPost1: Post = {
             id: 1,
             text: "Post content",
             author: "M",
             title: "First Post",
             topics: ["technology", "programming"]
-            };
-        return [mockPost];
+        };
+
+        const mockPost2: Post = {
+            id: 2,
+            text: "This post was created with CI/CD",
+            author: "J",
+            title: "Second Post",
+            topics: ["cloud", "rso"]
+        };
+
+        return [mockPost1, mockPost2];
     }
 }
